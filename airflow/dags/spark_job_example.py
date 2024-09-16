9b2384544a74e73de55ab233c5b07dde2d6a5b6c
zhuozhenzhao <zhuozhen.zhao@kylg.org>
2024-09-16 14:26:10 +0200

    add zippelin

--- airflow/dags/spark_job_example.py
+++ airflow/dags/spark_job_example.py
@@ -8,11 +8,14 @@ args = {
     'owner': 'airflow',
     'start_date': datetime(2021, 6, 28)
 }
-dag = DAG('spark_job', default_args=args, schedule_interval="@daily")
+dag = DAG('spark_job', default_args=args, schedule_interval="@daily", max_active_runs=1)
 
 scala_spark_example = SparkSubmitOperator(
     java_class="org.kylg.dbiv.AppRunner",
     application="hdfs:/tmp/edwin/dbivp1-1.0-SNAPSHOT-jar-with-dependencies.jar",
     task_id="submit_scala_spark_pi",
     name='airflowspark-submittest',
-    dag=dag)
+    dag=dag,
+    jars='/usr/spark/jars/spark-2.4-spline-agent-bundle_2.11-0.6.1.jar,/usr/spark/jars/commons-configuration-1.6.jar',
+    #packages='za.co.absa.spline.agent.spark:spark-2.4-spline-agent-bundle_2.12:0.6.1',
+    conf={'spark.sql.queryExecutionListeners':'za.co.absa.spline.harvester.listener.SplineQueryExecutionListener', 'spark.spline.lineageDispatcher.http.producer.url':'http://spline_server:8080/producer'})
